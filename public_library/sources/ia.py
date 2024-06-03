7ffed7c09d2c4431514f076595aad06ffd9b1e0b
Sebastian Majstorovic <storytracer@gmail.com>
2024-06-03 11:49:50 +0200

    public_library: implemented indexing and matching of downloaded books

--- public_library/sources/ia.py
+++ public_library/sources/ia.py
@@ -3,6 +3,7 @@ import json
 import multiprocessing.dummy as mp
 from pathlib import Path
 
+import click
 import duckdb
 import pandas as pd
 from internetarchive import ArchiveSession
@@ -14,43 +15,45 @@ SOURCE_PATH = Path(__file__).resolve().parent.parent
 METADATA_PATH = SOURCE_PATH / "data" / "metadata"
 IA_METADATA_PATH = METADATA_PATH / "ia"
 DATASET_NAME = "hathi_ia_pd_us_1929"
-DATASET_PATH = IA_METADATA_PATH / DATASET_NAME
+DATASET_METADATA_PATH = IA_METADATA_PATH / DATASET_NAME
+DATASET_BOOKS_PATH = SOURCE_PATH / "data" / "books" / "ia" / DATASET_NAME
 
 MAX_PARALLEL_DOWNLOADS = 10
 
 session = ArchiveSession()
-
 logger = logs.get_logger("public_library")
+
+
+@click.group()
+@click.option("--hathi-metadata-parquet", type=click.Path(exists=True), required=True)
+@click.pass_context
+def cli(ctx, hathi_metadata_parquet):
+    ctx.ensure_object(dict)
+    ctx.obj["hathi_metadata_parquet"] = hathi_metadata_parquet
+    logs.configure_logging("public_library")
 
 
 def fetch_metadata(ark_hash):
     query = f"identifier-ark:*{ark_hash}"
-    json_path = DATASET_PATH / f"{ark_hash}.json"
+    json_path = DATASET_METADATA_PATH / f"{ark_hash}.json"
 
     try:
         results = session.search_items(query=query, max_retries=3)
         for item in results:
-            if item:
-                ocaid = item.get("identifier", None)
-                if ocaid:
-                    metadata = session.get_item(ocaid).metadata
-                    if metadata and not json_path.exists():
-                        with open(json_path, "w") as f:
-                            json.dump(metadata, f)
-                        return True
-                else:
-                    logger.info(f"{ark_hash}: no OCAID in search results")
-            else:
-                logger.info(f"{ark_hash}: no search results")
+            ocaid = item.get("identifier")
+            if ocaid:
+                metadata = session.get_item(ocaid).metadata
+                if metadata and not json_path.exists():
+                    with open(json_path, "w") as f:
+                        json.dump(metadata, f)
+                    return True
     except Exception as e:
         logger.error(f"{ark_hash}: {str(e)}")
-        pass
-
     return False
 
 
 def hashes_to_download(ark_hashes):
-    existing_hashes = [path.stem for path in DATASET_PATH.glob("*.json")]
+    existing_hashes = {path.stem for path in DATASET_METADATA_PATH.glob("*.json")}
     return [ark_hash for ark_hash in ark_hashes if ark_hash not in existing_hashes]
 
 
@@ -66,47 +69,120 @@ def download_metadata_for_ark_hashes(ark_hashes):
     with tqdm(total=len(ark_hashes), initial=downloaded_hashes_count) as pbar:
         with mp.Pool(MAX_PARALLEL_DOWNLOADS) as pool:
             results = pool.imap(functools.partial(fetch_metadata), outstanding_hashes)
-            for result in results:
+            for _ in results:
                 pbar.update(1)
 
 
-def merge_metadata_files():
-    metadata_files = IA_METADATA_PATH.glob("*.json")
-    metadata_jsonl_file = IA_METADATA_PATH / f"{DATASET_NAME}.jsonl"
-    for file in metadata_files:
-        with open(file, "r") as f:
-            with open(metadata_jsonl_file, "a") as out:
-                json.dump(json.load(f), out)
-                out.write("\n")
+@cli.command()
+def export_jsonl():
+    metadata_files = DATASET_METADATA_PATH.glob("*.json")
+    metadata_jsonl_path = IA_METADATA_PATH / f"{DATASET_NAME}.jsonl"
+    print(metadata_jsonl_path)
+    with open(metadata_jsonl_path, "w") as metadata_jsonl_file:
+        for metadata_file_path in metadata_files:
+            with open(metadata_file_path, "r") as metadata_file:
+                data = json.dumps(json.load(metadata_file))
+                metadata_jsonl_file.write(data + "\n")
 
 
-def export_parquet():
+@cli.command()
+@click.pass_context
+def export_duckdb(ctx):
+    ia_metadata_jsonl_path = IA_METADATA_PATH / f"{DATASET_NAME}.jsonl"
     db = IA_METADATA_PATH / f"{DATASET_NAME}.duckdb"
-    parquet_path = IA_METADATA_PATH / f"{DATASET_NAME}.parquet"
+    db.unlink(missing_ok=True)
     con = duckdb.connect(str(db))
-    con.execute(f"COPY ia_metadata TO '{parquet_path}'")
+    downloaded_ark_hashes = [
+        metadata_file.stem for metadata_file in DATASET_METADATA_PATH.glob("*.json")
+    ]
+    downloaded_ocaids = [
+        path.stem.split("_djvu")[0] for path in DATASET_BOOKS_PATH.glob("*_djvu.txt")
+    ]
+    downloaded_ark_hashes_df = pd.DataFrame(downloaded_ark_hashes, columns=["ark_hash"])
+    downloaded_ocaids_df = pd.DataFrame(downloaded_ocaids, columns=["ocaid"])
+    con.execute(
+        f"CREATE TABLE metadata_hathi AS SELECT * FROM '{ctx.obj['hathi_metadata_parquet']}'"
+    )
+    con.execute(
+        f"CREATE TABLE metadata_ia AS SELECT * FROM read_json('{ia_metadata_jsonl_path}', ignore_errors=True, union_by_name=True)"
+    )
+    con.execute(
+        "CREATE TABLE metadata_ids (ark_hash VARCHAR, htid VARCHAR, ia_ark VARCHAR, ocaid VARCHAR)"
+    )
+    con.execute(
+        "CREATE TABLE downloads (ocaid VARCHAR, htid VARCHAR, title VARCHAR, author VARCHAR, year INT)"
+    )
+    con.execute(
+        "INSERT INTO metadata_ids (ark_hash) SELECT ark_hash FROM downloaded_ark_hashes_df"
+    )
+    con.execute(
+        "UPDATE metadata_ids SET htid = (SELECT htid FROM metadata_hathi WHERE ends_with(htid, ark_hash) = TRUE)"
+    )
+    con.execute(
+        'UPDATE metadata_ids SET ia_ark = (SELECT "identifier-ark" FROM metadata_ia WHERE ends_with("identifier-ark", ark_hash) = TRUE)'
+    )
+    con.execute(
+        'UPDATE metadata_ids SET ocaid = (SELECT identifier FROM metadata_ia WHERE "identifier-ark" = ia_ark)'
+    )
+    con.execute("INSERT INTO downloads (ocaid) SELECT ocaid FROM downloaded_ocaids_df")
+    con.execute(
+        "UPDATE downloads SET htid = (SELECT htid FROM metadata_ids WHERE ocaid = downloads.ocaid)"
+    )
+    con.execute(
+        "UPDATE downloads SET title = (SELECT title FROM metadata_hathi WHERE htid = downloads.htid)"
+    )
+    con.execute(
+        "UPDATE downloads SET author = (SELECT author FROM metadata_hathi WHERE htid = downloads.htid)"
+    )
+    con.execute(
+        "UPDATE downloads SET year = (SELECT rights_date_used FROM metadata_hathi WHERE htid = downloads.htid)"
+    )
     con.close()
 
 
-def generate_download_links():
+@cli.command()
+def export_parquet():
+    db = IA_METADATA_PATH / f"{DATASET_NAME}.duckdb"
+    con = duckdb.connect(str(db))
+    con.execute(
+        f"COPY metadata_ids TO '{IA_METADATA_PATH / (DATASET_NAME + '_metadata_ids.parquet')}'"
+    )
+    con.execute(
+        f"COPY metadata_hathi TO '{IA_METADATA_PATH / (DATASET_NAME + '_metadata_hathi.parquet')}'"
+    )
+    con.execute(
+        f"COPY metadata_ia TO '{IA_METADATA_PATH / (DATASET_NAME + '_metadata_ia.parquet')}'"
+    )
+    con.execute(
+        f"COPY downloads TO '{IA_METADATA_PATH / (DATASET_NAME + '_downloads.parquet')}'"
+    )
+    con.close()
+
+
+@cli.command()
+def to_dolma():
+    pass
+
+
+@cli.command()
+def generate_links():
     parquet_path = IA_METADATA_PATH / f"{DATASET_NAME}.parquet"
     df = pd.read_parquet(parquet_path, columns=["identifier"])
-    ocaids = df["identifier"].tolist()
     link_template = "https://archive.org/download/{ocaid}/{ocaid}_djvu.txt"
-    links = [link_template.format(ocaid=ocaid) for ocaid in ocaids]
     with open(IA_METADATA_PATH / f"{DATASET_NAME}_links.txt", "w") as f:
-        for link in links:
-            f.write(f"{link}\n")
+        for ocaid in df["identifier"]:
+            link = link_template.format(ocaid=ocaid)
+            f.write(link + "\n")
 
 
-def download_metadata_for_hathitrust_parquet(parquet_file):
+def download_metadata(parquet_file):
     try:
         df = pd.read_parquet(parquet_file)
         ark_hashes = df["htid"].apply(lambda x: x.split("/")[-1]).tolist()
         download_metadata_for_ark_hashes(ark_hashes)
     except Exception as e:
-        print(f"An error occurred processing {parquet_file}: {str(e)}")
+        logger.error(f"An error occurred processing {parquet_file}: {str(e)}")
 
 
 if __name__ == "__main__":
-    logs.configure_logging("public_library")
+    cli()
