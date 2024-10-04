69601fa664d3311b2760c4cc22517c5ed90d80b8
Sebastian Majstorovic <storytracer@gmail.com>
2024-10-04 18:15:35 +0200

    Adjusted download script to new record structure

--- usgpo/download-files.py
+++ usgpo/download-files.py
@@ -4,9 +4,10 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import jsonlines
 import trafilatura
+import requests
+from furl import furl
 from bs4 import BeautifulSoup
 from tqdm.auto import tqdm
-from utils import api_query
 
 from licensed_pile import logs
 from licensed_pile.licenses import PermissiveLicenses
@@ -14,10 +15,9 @@ from licensed_pile.write import to_dolma
 
 SOURCE_NAME = "usgpo"
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--api-key", required=True, help="GovInfo API key")
+    parser.add_argument("--mirror-url", required=False, help="URL to GovInfo file mirror")
     parser.add_argument(
         "--links-file", required=True, help="Path to links file (jsonl)"
     )
@@ -39,8 +39,32 @@ def parse_args():
     return args
 
 
-def download_file(api_key, file_url):
-    response = api_query(file_url, headers=None, params={"api_key": api_key})
+def download_file(file_url, mirror_url):
+    response = None
+    
+    if mirror_url is not None:
+        file_furl = furl(file_url)
+        mirror_furl = furl(mirror_url)
+        
+        file_path = file_furl.path
+        mirror_furl.path = mirror_furl.path / file_path
+        
+        try:
+            response = requests.get(mirror_furl.url)
+        except Exception as e:
+            response = None
+            raise Exception(f"Could not download file from mirror: {mirror_furl.url}")
+        
+    if response is None or response.status_code != 200:
+        try:
+            response = requests.get(file_url)
+        except Exception as e:
+            response = None
+            raise Exception(f"Could not download file from govinfo.gov: {file_url}")
+        
+    if response is None or response.status_code != 200:
+        return None
+
     text = response.text
     return text
 
@@ -51,61 +75,87 @@ def parse_html(html):
     soup = BeautifulSoup(html, "html.parser")
     pre_tag = soup.find("pre")
     if pre_tag:
-        text = pre_tag.get_text()
+        text = pre_tag.get_text().strip()
     else:
         text = trafilatura.extract(html, output_format="markdown")
     return text
 
 
-def construct_record(api_key, file):
+def construct_record(mirror_url, file):
     logger = logs.get_logger("usgpo")
+    records = []
+    
     try:
-        links = file.get("links")
-        if links is None:
-            return None
-
-        file_url = links.get("txtLink")
-        # Occassionally there will be multiple txtLinks pointing to the same URL. Just take the first.
-        if isinstance(file_url, list):
-            file_url = file_url[0]
-
-        if file_url is None:
-            return None
-
-        html = download_file(api_key, file_url)
-        text = parse_html(html)
-
-        if text is None or len(text) == 0:
-            return None
-
-        return {
-            "id": file["package_id"],
-            "title": file["title"],
-            "date": file["date"],
-            "author": file["author"],
-            "publisher": file["publisher"],
-            "category": file["category"],
-            "text": text,
-            "source": SOURCE_NAME,
-            "added": datetime.datetime.utcnow().isoformat(),
-            "metadata": {"license": str(PermissiveLicenses.PD), "url": file_url},
-        }
-
+        html_links = file.get("html_links")
+        if html_links is None or len(html_links) == 0:
+            return records
+        
+        if len(html_links) == 1:
+            link = html_links[0]
+            
+            html = download_file(link, mirror_url)
+            text = parse_html(html)
+            
+            record = {
+                "collection": file["collection"],
+                "id": file["package_id"],
+                "title": file["title"],
+                "date": file["date"],
+                "author": file["author"],
+                "publisher": file["publisher"],
+                "text": text,
+                "source": SOURCE_NAME,
+                "added": datetime.datetime.utcnow().isoformat(),
+                "metadata": {"license": str(PermissiveLicenses.PD), "url": link},
+            }
+            records.append(record)
+        elif len(html_links) > 1:
+            granules = file.get("granules")
+            if granules is None:
+                return records
+            
+            for granule in granules:
+                links = granule.get("links")
+                if links:
+                    html_link = links.get("HTML rendition")
+                    if html_link:
+                        link = html_link
+                        
+                        html = download_file(link, mirror_url)
+                        text = parse_html(html)
+                
+                        record = {
+                            "collection": file["collection"],
+                            "id": granule["granule_id"],
+                            "title": file["title"],
+                            "date": file["date"],
+                            "author": file["author"],
+                            "publisher": file["publisher"],
+                            "text": text,
+                            "source": SOURCE_NAME,
+                            "added": datetime.datetime.utcnow().isoformat(),
+                            "metadata": {"license": str(PermissiveLicenses.PD), "url": link},
+                        }
+                        
+                        records.append(record)
     except Exception as e:
         logger.error(f"Failed to download package {file['package_id']}: {e}")
-        return None
+        return records
+                
+    return records
 
 
 def generate_records(args):
     with jsonlines.open(args.links_file, mode="r") as reader:
         with ThreadPoolExecutor(max_workers=args.workers) as executor:
             futures = [
-                executor.submit(construct_record, args.api_key, file) for file in reader
+                executor.submit(construct_record, args.mirror_url, file) for file in reader
             ]
             for future in as_completed(futures):
-                record = future.result()
-                if record is not None:
-                    yield record
+                records = future.result()
+                for record in records:
+                    if record is not None:
+                        yield record
 
 
 def main(args):
